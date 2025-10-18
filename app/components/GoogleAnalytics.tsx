d6aef2697a0af287ef63024dfcca5f56ad93b5f1
Sasmitha Silva <silvasasmitha53@gmail.com>
2025-10-19 00:04:38 +0530

    Google Analytics Added

---
+++ app/components/GoogleAnalytics.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Script from "next/script";
+import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+
+interface GoogleAnalyticsProps {
+  measurementId: string;
+}
+
+// Extend Window interface to include gtag
+declare global {
+  interface Window {
+    gtag?: (
+      command: string,
+      targetId: string,
+      config?: Record<string, unknown>
+    ) => void;
+    dataLayer?: unknown[];
+  }
+}
+
+export default function GoogleAnalytics({ measurementId }: GoogleAnalyticsProps) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (!measurementId) return;
+
+    // Track page views on route change
+    const url = pathname + searchParams.toString();
+    
+    // Check if gtag is available
+    if (typeof window !== "undefined" && window.gtag) {
+      window.gtag("config", measurementId, {
+        page_path: url,
+      });
+    }
+  }, [pathname, searchParams, measurementId]);
+
+  if (!measurementId) {
+    return null;
+  }
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${measurementId}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+}
